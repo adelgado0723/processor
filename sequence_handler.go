770623bb94f09c64ac6f018cabf2ccd24a4aca7e
Andres Delgado <adelgado0723@gmail.com>
2022-01-27 10:04:41 -0500

    1:00:31 Episode 5 - Building the Sequencer

--- sequence_handler.go
+++ sequence_handler.go
@@ -1,20 +1,35 @@
 package processor
 
 type SequenceHandler struct {
-	input  chan *Envelope
-	output chan *Envelope
+	input       chan *Envelope
+	output      chan *Envelope
+	envelopeIdx int
+	buffer      map[int]*Envelope
 }
 
 func NewSequenceHandler(input, output chan *Envelope) *SequenceHandler {
 	return &SequenceHandler{
 		input:  input,
 		output: output,
+		buffer: make(map[int]*Envelope),
 	}
 }
 
+func (sh *SequenceHandler) sendBufferedEnvelopesInOrder() {
+	_, found := sh.buffer[sh.envelopeIdx]
+	for ; found; _, found = sh.buffer[sh.envelopeIdx] {
+		sh.output <- sh.buffer[sh.envelopeIdx]
+		delete(sh.buffer, sh.envelopeIdx)
+		sh.envelopeIdx++
+	}
+}
+func (sh *SequenceHandler) processEnvelope(envelope *Envelope) {
+	sh.buffer[envelope.Sequence] = envelope
+	sh.sendBufferedEnvelopesInOrder()
+}
+
 func (sh *SequenceHandler) Handle() {
-	for i := 0; i < 2; i++ {
-		input := <-sh.input
-		sh.output <- input
+	for envelope := range sh.input {
+		sh.processEnvelope(envelope)
 	}
 }
--- sequence_handler_test.go
+++ sequence_handler_test.go
@@ -26,23 +26,45 @@ func (shf *SequenceHandlerFixture) Setup() {
 func (shf *SequenceHandlerFixture) TestExpectedEnvelopeSentToOutput() {
 	envelope := &Envelope{Sequence: 0}
 	shf.input <- envelope
+	close(shf.input)
 	shf.handler.Handle()
 
 	shf.AssertEqual(<-shf.output, envelope)
 }
 
 func (shf *SequenceHandlerFixture) TestEvelopeReceivedOutOfOrder_BufferedUntilContiguousBlock() {
-	envelope0 := &Envelope{Sequence: 0}
-	envelope1 := &Envelope{Sequence: 1}
-
 	// Invalid order
-	shf.input <- envelope1
-	shf.input <- envelope0
+	shf.input <- &Envelope{Sequence: 4}
+	shf.input <- &Envelope{Sequence: 1}
+	shf.input <- &Envelope{Sequence: 2}
+	shf.input <- &Envelope{Sequence: 3}
+	shf.input <- &Envelope{Sequence: 0}
 
+	close(shf.input)
 	shf.handler.Handle()
 
-	// Expecting envelope0 first despite invalid order
-	shf.AssertEqual(<-shf.output, envelope0)
-	shf.AssertEqual(<-shf.output, envelope1)
+	close(shf.output)
 
+	shf.assertSequenceOrder(shf.sequenceOrder(), []int{0, 1, 2, 3, 4})
+	// Expecting envelope0 first despite invalid order
+	// shf.AssertEqual((<-shf.output).Sequence, 0)
+	// shf.AssertEqual((<-shf.output).Sequence, 1)
+	// shf.AssertEqual((<-shf.output).Sequence, 2)
+	// shf.AssertEqual((<-shf.output).Sequence, 3)
+	// shf.AssertEqual((<-shf.output).Sequence, 4)
+	shf.AssertEqual(len(shf.handler.buffer), 0)
+}
+
+func (shf *SequenceHandlerFixture) sequenceOrder() (order []int) {
+	for envelope := range shf.output {
+		order = append(order, envelope.Sequence)
+	}
+	return order
+}
+func (shf *SequenceHandlerFixture) assertSequenceOrder(actual, expected []int) {
+
+	shf.AssertEqual(len(actual), len(expected))
+	for index, seqNo := range actual {
+		shf.AssertEqual(expected[index], seqNo)
+	}
 }
