ae4133d563cfb8d74c69e8198b94bf19fb4eb94b
Andres Delgado <adelgado0723@gmail.com>
2022-01-31 20:14:36 -0500

    52:06 Episode 6 - Starting the writer

--- writer_handler.go
+++ writer_handler.go
@@ -12,28 +12,35 @@ type WriterHandler struct {
 }
 
 func NewWriterHandler(input chan *Envelope, output io.WriteCloser) *WriterHandler {
-	return &WriterHandler{
+	wh := &WriterHandler{
 		input:  input,
 		closer: output,
 		writer: csv.NewWriter(output),
 	}
-
+	wh.writer.Write([]string{"Status", "DeliveryLine1", "LastLine", "City", "State", "ZIPCode"})
+	return wh
 }
 func (wh *WriterHandler) Handle() {
-	wh.writer.Write([]string{"Status", "DeliveryLine1", "City", "State", "ZIPCode"})
 
-	envelope := <-wh.input
-	if envelope != nil {
+	for envelope := range wh.input {
 		output := envelope.Output
-		wh.writer.Write([]string{
-			output.Status,
-			output.DeliveryLine1,
-			output.City,
-			output.State,
-			output.ZIPCode,
-			output.LastLine,
-		})
+		wh.writeAddressOutput(output)
 	}
 	wh.writer.Flush()
 	wh.closer.Close()
+}
+
+func (wh *WriterHandler) writeValues(values ...string) {
+	wh.writer.Write(values)
+}
+
+func (wh *WriterHandler) writeAddressOutput(output AddressOutput) {
+	wh.writeValues(
+		output.Status,
+		output.DeliveryLine1,
+		output.LastLine,
+		output.City,
+		output.State,
+		output.ZIPCode,
+	)
 }
--- writer_handler_test.go
+++ writer_handler_test.go
@@ -3,6 +3,7 @@ package processor
 import (
 	"bytes"
 	"encoding/csv"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -23,7 +24,7 @@ type WriterHandlerFixture struct {
 
 func (whf *WriterHandlerFixture) Setup() {
 	whf.buffer = NewWriterSpyBuffer("")
-	whf.input = make(chan *Envelope)
+	whf.input = make(chan *Envelope, 10)
 	whf.handler = NewWriterHandler(whf.input, whf.buffer)
 }
 
@@ -31,7 +32,7 @@ func (whf *WriterHandlerFixture) TestHeaderWritten() {
 	close(whf.input)
 
 	whf.handler.Handle()
-	whf.AssertEqual(whf.buffer.String(), "Status,DeliveryLine1,City,State,ZIPCode\n")
+	whf.AssertEqual(whf.buffer.String(), "Status,DeliveryLine1,LastLine,City,State,ZIPCode\n")
 
 }
 
@@ -44,23 +45,71 @@ func (whf *WriterHandlerFixture) TestOutputClosed() {
 }
 
 func (whf *WriterHandlerFixture) TestEnvelopeWritten() {
-	whf.input <- &Envelope{
-		Output: AddressOutput{
-			Status:        "A",
-			DeliveryLine1: "B",
-			City:          "C",
-			State:         "D",
-			ZIPCode:       "E",
-			LastLine:      "F",
-		}}
-	close(whf.input)
+	whf.sendEnvelope(1)
 	whf.handler.Handle()
 
-	outputFile := strings.TrimSpace(whf.buffer.String())
-	lines := strings.Split(outputFile, "\n")
-
+	lines := whf.outputLines()
 	if whf.AssertEqual(len(lines), 2) {
-		whf.AssertEqual(lines[1], "A,B,C,D,E,F")
+		whf.AssertEqual(lines[1], "A1,B1,C1,D1,E1,F1")
+	}
+}
+
+func (whf *WriterHandlerFixture) TestAllEnvelopesWritten() {
+	whf.sendEnvelope(2)
+	whf.handler.Handle()
+
+	lines := whf.outputLines()
+	if whf.AssertEqual(len(lines), 3) {
+		whf.AssertEqual(lines[1], "A1,B1,C1,D1,E1,F1")
+		whf.AssertEqual(lines[2], "A2,B2,C2,D2,E2,F2")
+	}
+}
+
+var recordMatchingHeader = AddressOutput{
+	Status:        "Status",
+	DeliveryLine1: "DeliveryLine1",
+	LastLine:      "LastLine",
+	City:          "City",
+	State:         "State",
+	ZIPCode:       "ZIPCode",
+}
+
+func (whf *WriterHandlerFixture) TestHeaderMatchesRecords() {
+	whf.input <- &Envelope{
+		Output: recordMatchingHeader,
+	}
+	close(whf.input)
+	whf.handler.Handle()
+	whf.assertHeaderMatchesRecord()
+}
+
+func (whf *WriterHandlerFixture) assertHeaderMatchesRecord() {
+	lines := whf.outputLines()
+	whf.AssertEqual(lines[0], "Status,DeliveryLine1,LastLine,City,State,ZIPCode")
+	whf.AssertEqual(lines[0], lines[1])
+}
+func (whf *WriterHandlerFixture) outputLines() []string {
+
+	outputFile := strings.TrimSpace(whf.buffer.String())
+	return strings.Split(outputFile, "\n")
+}
+
+func (whf *WriterHandlerFixture) sendEnvelope(count int) {
+	for i := 1; i < count+1; i++ {
+		whf.input <- &Envelope{
+			Output: createOutput(strconv.Itoa(i)),
+		}
+	}
+	close(whf.input)
+}
+func createOutput(index string) AddressOutput {
+	return AddressOutput{
+		Status:        "A" + index,
+		DeliveryLine1: "B" + index,
+		LastLine:      "C" + index,
+		City:          "D" + index,
+		State:         "E" + index,
+		ZIPCode:       "F" + index,
 	}
 }
 
