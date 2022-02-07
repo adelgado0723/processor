3549e58c7059439de8d75a3ad270a2f9aef894ec
Andres Delgado <adelgado0723@gmail.com>
2022-02-07 08:20:32 -0500

    26:10 - Episode 8

--- wireup.go
+++ wireup.go
@@ -13,8 +13,10 @@ type Wireup struct {
 	client HTTPClient
 }
 
-func (w *Wireup) Build() {
-
+func (w *Wireup) Build() Handler {
+	verifyInput := make(chan *Envelope, 1024)
+	reorderInput := make(chan *Envelope, 1024)
+	writerInput := make(chan *Envelope, 1024)
 }
 
 func Configure(reader io.ReadCloser, writer io.WriteCloser, client HTTPClient) *Wireup {
--- wireup_test.go
+++ wireup_test.go
@@ -1,6 +1,7 @@
 package processor
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/smartystreets/gunit"
@@ -15,13 +16,42 @@ type WireupFixture struct {
 
 	reader  *ReadWriteSpyBuffer
 	writer  *ReadWriteSpyBuffer
+	client  *FakeHTTPClient
 	handler Handler
 }
 
 func (wf *WireupFixture) Setup() {
 	wf.reader = NewReadWriteSpyBuffer("")
 	wf.writer = NewReadWriteSpyBuffer("")
-	wf.handler = Configure(wf.reader, wf.writer, nil).Build()
+	wf.client = &FakeHTTPClient{}
+	wf.handler = Configure(wf.reader, wf.writer, wf.client).Build()
 
 }
-func (wf *WireupFixture) Test() {}
+func (wf *WireupFixture) LongTestPipeline() {
+	wf.client.Configure(integrationJSONOutput, http.StatusOK, nil)
+	wf.reader.WriteString("A,B,C,D")
+	wf.reader.WriteString("A,B,C,D")
+	wf.handler.Handle()
+	expected := "Status,DeliveryLine1,LastLine,City,State,ZIPCode\n" +
+		"Deliverable,AA,BB,CC,DD,EE\n" +
+		"Deliverable,AA,BB,CC,DD,EE\n"
+	wf.AssertEqual(expected, wf.writer.String())
+}
+
+const integrationJSONOutput = `
+[
+	{
+		"delivery_line_1": "AA",
+		"last_line": "BB",
+		"components": {
+			"city_name": "CC",
+			"state_abbreviation": "DD",
+			"zipcode": "EE"
+		},
+		"analysis": {
+			"dpv_match_code": "Y",
+			"dpv_vacant": "N",
+			"active": "Y"
+		}
+	}
+]`
